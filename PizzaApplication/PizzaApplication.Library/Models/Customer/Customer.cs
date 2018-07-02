56771d8e69d034d096d68f5503b0179ec35ccfd0
Chris Coronado <cbcoronado@live.com>
2018-07-02 16:53:33 -0400

    Added constructors to Customer class

--- PizzaApplication/PizzaApplication.Library/Models/Customer/Customer.cs
+++ PizzaApplication/PizzaApplication.Library/Models/Customer/Customer.cs
@@ -6,9 +6,25 @@ namespace PizzaApplication.Library
 {
     public class Customer
     {
+        // field
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string DefaultLocation { get; set; }
+        public string Location { get; set; }
+        public string DefaultLocation { get; set; } = "Reston";
         public Order CustomerOrder = new Order();
+
+        // methods
+        public Customer()
+        {
+
+        }
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
     }
 }
