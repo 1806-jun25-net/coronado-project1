ccb48478331fa2a50702780749fb945492185744
Chris Coronado <cbcoronado@live.com>
2018-07-06 21:13:12 -0400

    Added constructors and methods: NameOrder, AddPizza, UpdateTime, UpdatePrice, BuildOrder, and PrintOrder

--- PizzaApplication/PizzaApplication.Library/Models/Customer/Order.cs
+++ PizzaApplication/PizzaApplication.Library/Models/Customer/Order.cs
@@ -6,13 +6,77 @@ namespace PizzaApplication.Library
 {
     public class Order
     {
+        // fields and properties
+        public Customer OrderCustomer { get; set; }
         public string OrderName { get; set; }
-        public string OrderLocation { get; set; }
+        public string OrderLocation { get; set; } = "Store Location";
         public DateTime OrderTime { get; set; }
-        public decimal OrderPrice { get; set; }
-        public IEnumerable<Pizza> PizzaList { get; set; }
+        public decimal OrderPrice { get; set; } = 0.00m;
+        public decimal OrderPriceLimit { get; set; } = 150.00m; //made limit $150 because the $500 limit in the project requirements was too high for my pricing model
+        public int OrderPizzaCount { get; set; } = 0;
+        public int OrderPizzaLimit { get; set; } = 12;
+        public IEnumerable<Pizza> PizzaList = new List<Pizza>();
 
+        // constructors
+        public Order()
+        {            
+        }
 
+        public Order(Customer customer, string location)
+        {
+            OrderCustomer = customer;
+            OrderLocation = location;
+            OrderTime = DateTime.Now;
+            NameOrder();
+        }
 
+        // methods
+        public void NameOrder()
+        {            
+            OrderName = $"{OrderPizzaCount} Pizza(s), ${OrderPrice}, to {OrderLocation}, for {OrderCustomer.FirstName} {OrderCustomer.LastName}, at {OrderTime}";
+        }
+
+        public void AddPizza(Pizza pizza)
+        {
+            var pizzaList = (List<Pizza>)PizzaList;
+            pizzaList.Add(pizza);
+            OrderPrice += pizza.PizzaPrice;
+            OrderPizzaCount++;
+        }
+
+        public void UpdateTime()
+        {
+            OrderTime = DateTime.Now;
+        }
+
+        public void UpdatePrice()
+        {
+            // reset price to $0.00 before recalculating
+            OrderPrice = 0.00m;
+            foreach (var pizza in PizzaList)
+            {
+                OrderPrice += pizza.PizzaPrice;
+            }
+        }
+
+        public void BuildOrder()
+        {
+            UpdatePrice();
+            UpdateTime();
+            NameOrder();
+        }
+
+        public void PrintOrder()
+        {
+            Console.WriteLine($"\nOrder:" +
+                $"\n{OrderName}" +
+                $"\n---" +
+                $"\nPizza List:" +
+                $"\n---");
+            foreach (var pizza in PizzaList)
+            {
+                pizza.PrintPizza();
+            }
+        }
     }
 }
