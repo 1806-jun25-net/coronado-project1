9e5f802f2c161d761ca46d15d485c610278dcaf2
Chris Coronado <cbcoronado@live.com>
2018-07-06 21:17:59 -0400

    Added fields and properties and shell for CheckIfOrderCanBeFulfilled method

--- PizzaApplication/PizzaApplication.Library/Models/Storefront/Storefront.cs
+++ PizzaApplication/PizzaApplication.Library/Models/Storefront/Storefront.cs
@@ -6,5 +6,19 @@ namespace PizzaApplication.Library
 {
     public class Storefront
     {
+        // fields and properties
+        public string StoreLocation { get; set; }
+        public Inventory StoreInventory { get; set; }
+        public StorefrontOrderHistory StoreOrderHistory { get; set; }
+
+        // methods
+        public bool CheckIfOrderCanBeFulfilled(Order order)
+        {
+            var check = false;
+
+
+
+            return check;
+        }
     }
 }
