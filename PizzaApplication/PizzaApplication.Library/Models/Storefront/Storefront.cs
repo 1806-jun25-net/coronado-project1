9e3a1ec6b821d0158d752c4e401300a164d79999
Chris Coronado <cbcoronado@live.com>
2018-07-09 15:51:48 -0400

    Added order checking and inventory setting methods to Storefront

--- PizzaApplication/PizzaApplication.Library/Models/Storefront/Storefront.cs
+++ PizzaApplication/PizzaApplication.Library/Models/Storefront/Storefront.cs
@@ -9,7 +9,7 @@ namespace PizzaApplication.Library
         // fields and properties
         public string StoreLocation { get; set; }
         public Inventory StoreInventory { get; set; } = new Inventory();
-        public StorefrontOrderHistory StoreOrderHistory { get; set; }
+        public StorefrontOrderHistory StoreOrderHistory { get; set; } = new StorefrontOrderHistory();
 
         // constructors
         public Storefront(string location)
@@ -18,13 +18,79 @@ namespace PizzaApplication.Library
         }
 
         // methods
-        public bool CheckIfInventoryIsSufficient(Order order)
+        public bool CanOrderBeFulfilled(Order order)
         {
-            var check = false;
+            var tempCheck = true;
 
+            // reset temp inventory back to actual amounts before the individual deductions from the pizza builder phase
+            StoreInventory.SetTempInventoryToActual();
+            foreach (var pizza in order.PizzaList)
+            {
+                foreach (var ingredient in pizza.PizzaComposition) // deduct ingredient costs from storefront's actual inventory
+                {
+                    // loop through and deduct from temp inventory
+                    if (StoreInventory.CheckIfInventoryIsSufficient(ingredient.IngredientInventoryName, ingredient.IngredientInventoryCost))
+                    {
+                        StoreInventory.DeductTempInventoryCount(ingredient.IngredientInventoryName, ingredient.IngredientInventoryCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sorry, there is not enough {ingredient.IngredientInventoryName} in the {StoreLocation} storefront's inventory.");
+                        tempCheck = false;
+                    }
+                }
+            }                       
+            return tempCheck;
+        }
 
+        public void SetInventory(bool tempCheck)
+        {
+            if (tempCheck) // if temp inventory deductions pass, then deduct from actual inventory
+            {
+                // set actual inventory to temp inventory values
+                StoreInventory.SetActualInventoryToTemp();
+            }
+            else
+            {
+                // if temp inventory deductions don't pass, reset temp inventory to actual inventory valuess
+                StoreInventory.SetTempInventoryToActual();
+            }
+        }
 
-            return check;
+        public void ProcessInventory(Order order)
+        {
+            var check = CanOrderBeFulfilled(order);
+            SetInventory(check);
+        }
+
+        public void PrintInventory()
+        {
+            var nameList = (List<string>)StoreInventory.InventoryNameList;
+            var countList = (List<double>)StoreInventory.InventoryCountList;            
+            int index;
+
+            Console.WriteLine($"\n{StoreLocation} Storefront Inventory: \n---\n");
+        
+            foreach (var item in nameList)
+            {
+                index = nameList.IndexOf(item);
+                Console.WriteLine($"{item}: {countList[index]}");
+            }
+        }
+
+        public void PrintTempInventory()
+        {
+            var nameList = (List<string>)StoreInventory.InventoryNameList;
+            var countList = (List<double>)StoreInventory.TempInventoryCountList;
+            int index;
+
+            Console.WriteLine($"\n{StoreLocation} Storefront Temp Inventory: \n---\n");
+
+            foreach (var item in nameList)
+            {
+                index = nameList.IndexOf(item);
+                Console.WriteLine($"{item}: {countList[index]}");
+            }
         }
     }
 }
