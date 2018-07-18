e8a0e4ed5a206c8e496207e254feaab710947401
Chris Coronado <cbcoronado@live.com>
2018-07-18 02:31:42 -0400

    Added range restriction on inventory model

--- PizzaApp/PizzaApp.WebApp/Models/Inventory.cs
+++ PizzaApp/PizzaApp.WebApp/Models/Inventory.cs
@@ -12,33 +12,57 @@ namespace PizzaApp.WebApp
         public int Id { get; set; }
         [Display(Name = "Location Id")]
         public int? LocationId { get; set; }
+        [Range(0.00,200.00)]
         public double Dough { get; set; }
+        [Range(0.00, 100.00)]
         [Display(Name = "Tomato Sauce")]
         public double TomatoSauce { get; set; }
-        [Display(Name = "White Sauce")]
+        [Range(0.00, 100.00)]
+        [Display(Name = "White Sauce")]        
         public double WhiteSauce { get; set; }
+        [Range(0.00, 200.00)]
         public double Cheese { get; set; }
+        [Range(0.00, 50.00)]
         public double Pepperoni { get; set; }
+        [Range(0.00, 50.00)]
         public double Ham { get; set; }
+        [Range(0.00, 50.00)]
         public double Chicken { get; set; }
+        [Range(0.00, 50.00)]
         public double Beef { get; set; }
+        [Range(0.00, 50.00)]
         public double Sausage { get; set; }
+        [Range(0.00, 50.00)]
         public double Bacon { get; set; }
+        [Range(0.00, 50.00)]
         public double Anchovies { get; set; }
+        [Range(0.00, 50.00)]
         [Display(Name = "Red Peppers")]
         public double RedPeppers { get; set; }
+        [Range(0.00, 50.00)]
         [Display(Name = "Green Peppers")]
         public double GreenPeppers { get; set; }
+        [Range(0.00, 50.00)]
         public double Pineapple { get; set; }
+        [Range(0.00, 50.00)]
         public double Olives { get; set; }
+        [Range(0.00, 50.00)]
         public double Mushrooms { get; set; }
+        [Range(0.00, 50.00)]
         public double Garlic { get; set; }
+        [Range(0.00, 50.00)]
         public double Onions { get; set; }
+        [Range(0.00, 50.00)]
         public double Tomatoes { get; set; }
+        [Range(0.00, 50.00)]
         public double Spinach { get; set; }
+        [Range(0.00, 50.00)]
         public double Basil { get; set; }
+        [Range(0.00, 50.00)]
         public double Ricotta { get; set; }
+        [Range(0.00, 50.00)]
         public double Parmesan { get; set; }
+        [Range(0.00, 50.00)]
         public double Feta { get; set; }
     }
 }
